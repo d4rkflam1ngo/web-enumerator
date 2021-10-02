42f16be8f4e85d3087df9fffce41380b0d371f2a
Jamie Pegg <jamiepegg@zoho.com>
2021-10-02 17:25:34 +0100

    Started to add directory scanner

---
+++ scripts/directory.py
@@ -0,0 +1,24 @@
+# Import required modules
+import requests
+
+def directory(url, verbose):
+    # Read words from wordlist
+    wordlist = open("wordlists/directory-common.txt", "r")
+
+    # File extensions to check
+    extensions = ["", ".txt", ".php", ".html", ".js"]
+
+    for word in wordlist:
+        for extension in extensions:
+
+            # Make request to the url + directory + extension
+            request = requests.get(url + "/" + word.strip() + extension)
+
+            # If verbose is True then print all results
+            if verbose == True:
+                print("["+str(request.status_code)+"] " + url + "/" + word.strip() + extension)
+
+            # If not verbose then only print results which are not 404
+            elif request.status_code != 404:
+                print("["+str(request.status_code)+"] " + url + "/" + word.strip() + extension)
+    
--- web-enumerator.py
+++ web-enumerator.py
@@ -1,6 +1,7 @@
 # Imoprt required modules
 import argparse
 import re
+from scripts.directory import directory
 
 # Create a function which checks if the given url is valid
 def url_type(arg_value, pattern=re.compile("http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+")):
@@ -15,6 +16,9 @@ parser = argparse.ArgumentParser(description="A simple program which enumerates 
 
 # Create the arguments
 parser.add_argument("-u", "--url", type=url_type, metavar="", help="url of the site to enumerate", required=True)
+parser.add_argument("-v", "--verbose", action="store_true", help="enable verbose mode", required=False)
 
 # Parse the arguments
 args = parser.parse_args()
+
+directory(args.url, args.verbose)
